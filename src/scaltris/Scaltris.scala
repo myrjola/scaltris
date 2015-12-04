0164b7afd0e13e0bf557e3bb149f8e1ef9c68707
Martin Yrjölä <martin.yrjola@gmail.com>
2015-12-04 23:53:38 +0200

    Add menubar

--- src/scaltris/Scaltris.scala
+++ src/scaltris/Scaltris.scala
@@ -1,7 +1,12 @@
 package scaltris
 
+import scala.swing.Action
 import scala.swing.Dimension
 import scala.swing.MainFrame
+import scala.swing.Menu
+import scala.swing.MenuBar
+import scala.swing.MenuItem
+import scala.swing.Separator
 import scala.swing.SimpleSwingApplication
 
 object Scaltris extends SimpleSwingApplication {
@@ -14,6 +19,15 @@ object Scaltris extends SimpleSwingApplication {
                                   Block.BlockSize * Board.Height + 20)
 
     contents = boardPanel
+
+    menuBar = new MenuBar {
+      contents += new Menu("Game") {
+        contents += new MenuItem(Action("New game") { boardPanel.controller.newGame })
+        contents += new MenuItem(Action("Pause") { boardPanel.controller.togglePause })
+        contents += new Separator
+        contents += new MenuItem(Action("Exit") { sys.exit(0) })
+      }
+    }
 
     boardPanel.requestFocus
   }
